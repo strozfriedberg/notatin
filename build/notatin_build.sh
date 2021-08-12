5a9863381e155a2232a362ca87c57dbd7d90fa25
Kimberly Stone <kimberly.stone@aon.com>
2021-08-12 16:22:28 -0700

    Correct fix for pyo3/cargo test incompatibility

--- build/notatin_build.sh
+++ build/notatin_build.sh
@@ -4,7 +4,7 @@ cargo test
 cargo clippy --all-features --all-targets
 
 cd pyreg
-cargo test
+cargo test --no-default-features
 cargo clippy --all-features --all-targets
 poetry run maturin develop
 poetry run pytest
