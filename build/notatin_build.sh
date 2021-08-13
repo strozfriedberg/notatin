1f04370fe7be9d958b6eaaa7893aa357b01c8197
Kimberly Stone <kimberly.stone@aon.com>
2021-08-13 11:05:19 -0700

    Explain cargo test option for pynotatin

--- build/notatin_build.sh
+++ build/notatin_build.sh
@@ -4,7 +4,7 @@ cargo test
 cargo clippy --all-features --all-targets
 
 cd pynotatin
-cargo test --no-default-features
+cargo test --no-default-features # --no-default-features is specified to avoid a bug in pyo3 (https://aeshirey.github.io/code/2020/04/01/tests-and-pyo3.html)
 cargo clippy --all-features --all-targets
 poetry run maturin develop
 poetry run pytest
