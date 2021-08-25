239278f763c7fb2e28fe1d8784233f1f8818975d
Kimberly Stone <kimberly.stone@aon.com>
2021-08-25 16:19:15 -0700

    Add support for recovering unused keys and values from a primary registry file

---
+++ src/cell.rs
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Aon Cyber Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use nom::{alt, named, tag};
+use serde::Serialize;
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+pub enum CellState {
+    Allocated,
+    ModifiedTransactionLog,
+    DeletedTransactionLog,
+    DeletedPrimaryFile,
+    DeletedPrimaryFileSlack,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize)]
+pub enum CellType {
+    CellOther,
+    CellKey,
+    CellValue,
+    CellSecurity,
+    CellBigData,
+    CellIndexRoot,
+    CellHashLeaf,
+    CellFastLeaf,
+    CellIndexLeaf,
+}
+
+impl CellType {
+    pub(crate) fn read_cell_type(input: &[u8]) -> Self {
+        named!(
+            cell_type<CellType>,
+            alt!(
+                tag!("nk") => { |_| CellType::CellKey } |
+                tag!("vk") => { |_| CellType::CellValue } |
+                tag!("sk") => { |_| CellType::CellSecurity } |
+                tag!("lf") => { |_| CellType::CellFastLeaf } |
+                tag!("li") => { |_| CellType::CellIndexLeaf } |
+                tag!("lh") => { |_| CellType::CellHashLeaf } |
+                tag!("ri") => { |_| CellType::CellIndexRoot } |
+                tag!("db") => { |_| CellType::CellBigData }
+            )
+        );
+        match cell_type(input) {
+            Ok((_, cell_type)) => cell_type,
+            Err(_) => CellType::CellOther,
+        }
+    }
+}
