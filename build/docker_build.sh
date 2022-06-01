ba4fc73a311db33d5371500b2d4be4aec12ae461
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2022-06-01 17:13:37 +0100

    Removed Jennkins cruft.

--- build/docker_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-
-IMAGE_NAME=$1
-
-docker build -f build/Dockerfile -t $IMAGE_NAME .
--- build/jenkins_build.sh
+++
@@ -1,5 +0,0 @@
-IMAGE_NAME=notatin
-
-./build/docker_build.sh $IMAGE_NAME
-docker run $IMAGE_NAME
-
