e487a7a302fbbb8e8b42dedc1412508420843bae
Kim Stone <kimberly.stone@aon.com>
2022-08-25 06:12:27 -0700

    R!! Move TsvWriter into file

--- bin/reg_dump/main.rs
+++ bin/reg_dump/main.rs
@@ -14,18 +14,15 @@
  * limitations under the License.
  */
 
-
+pub mod tsv_writer;
 pub mod xlsx_writer;
 
 use clap::{arg_enum, value_t, App, Arg};
 use notatin::{
-    cell::Cell,
-    cell_key_node::CellKeyNode,
-    cell_key_value::CellKeyValue,
     cli_util::parse_paths,
     err::Error,
-    filter::{Filter, FilterBuilder},
-    parser::{Parser, ParserIterator},
+    filter::FilterBuilder,
+    parser::ParserIterator,
     parser_builder::ParserBuilder,
     progress, util,
 };
@@ -33,6 +30,7 @@ use std::{
     fs::File,
     io::{BufWriter, Write},
 };
+use tsv_writer::WriteTsv;
 use xlsx_writer::WriteXlsx;
 
 fn main() -> Result<(), Error> {
@@ -136,112 +134,5 @@ arg_enum! {
         Common,
         Tsv,
         Xlsx
-    }
-}
-
-struct WriteTsv {
-    index: usize,
-    recovered_only: bool,
-    writer: BufWriter<File>,
-    console: Box<dyn progress::UpdateProgressTrait>,
-}
-
-impl WriteTsv {
-    fn new(output: &str, recovered_only: bool) -> Result<Self, Error> {
-        let write_file = File::create(output)?;
-        let writer = BufWriter::new(write_file);
-        Ok(WriteTsv {
-            index: 0,
-            recovered_only,
-            writer,
-            console: progress::new(true),
-        })
-    }
-
-    fn write(&mut self, parser: &Parser, filter: Option<Filter>) -> Result<(), Error> {
-        let mut iter = ParserIterator::new(parser);
-        if let Some(filter) = filter {
-            iter.with_filter(filter);
-        }
-
-        writeln!(self.writer,"Index\tKey Path\tValue Name\tValue Data\tTimestamp\tStatus\tPrevious Seq Num\tModifying Seq Num\tFlags\tAccess Flags\tValue Type\tLogs")?;
-        for (index, key) in iter.iter().enumerate() {
-            self.console.update_progress(index)?;
-            self.write_key_tsv(&key, false)?;
-        }
-        writeln!(self.writer, "\nLogs\n-----------")?;
-        parser.get_parse_logs().write::<File>(&mut self.writer)?;
-        Ok(())
-    }
-
-    fn write_value_tsv(
-        &mut self,
-        cell_key_node: &CellKeyNode,
-        value: &CellKeyValue,
-    ) -> Result<(), Error> {
-        if !self.recovered_only || value.has_or_is_recovered() {
-            self.index += 1;
-            writeln!(
-                self.writer,
-                "{index}\t{key_path}\t{value_name}\t{value_data}\t\t{status:?}\t{prev_seq_num}\t{mod_seq_num}\t\t\t{value_type}\t{logs}",
-                index = self.index,
-                key_path = util::escape_string(&cell_key_node.path),
-                value_name = util::escape_string(&value.get_pretty_name()),
-                value_data = util::escape_string(&value.get_content().0.to_string()),
-                status = value.cell_state,
-                prev_seq_num = Self::get_sequence_num_string(value.sequence_num),
-                mod_seq_num = Self::get_sequence_num_string(value.updated_by_sequence_num),
-                value_type = value.get_content().0.get_type(),
-                logs = util::escape_string(&value.logs.to_string())
-            )?;
-        }
-        Ok(())
-    }
-
-    fn write_key_tsv(
-        &mut self,
-        cell_key_node: &CellKeyNode,
-        key_modified: bool,
-    ) -> Result<(), Error> {
-        if !self.recovered_only || cell_key_node.has_or_is_recovered() {
-            let mut logs = cell_key_node.logs.clone();
-            self.index += 1;
-            writeln!(
-                self.writer,
-                "{index}\t{key_path}\t\t\t{timestamp}\t{status:?}\t{prev_seq_num}\t{mod_seq_num}\t{flags:?}\t{access_flags:?}\t\t{logs}",
-                index = self.index,
-                key_path = util::escape_string(&cell_key_node.path),
-                timestamp = util::format_date_time(cell_key_node.last_key_written_date_and_time()),
-                status = cell_key_node.cell_state,
-                prev_seq_num = Self::get_sequence_num_string(cell_key_node.sequence_num),
-                mod_seq_num = Self::get_sequence_num_string(cell_key_node.updated_by_sequence_num),
-                flags = cell_key_node.key_node_flags(&mut logs),
-                access_flags = cell_key_node.access_flags(&mut logs),
-                logs = util::escape_string(&cell_key_node.logs.to_string())
-            )?;
-
-            for sub_key in &cell_key_node.versions {
-                self.write_key_tsv(sub_key, true)?;
-            }
-        }
-
-        if !key_modified {
-            // don't output values for modified keys; current/modified/deleted vals will be output via the current version of the key
-            for value in cell_key_node.value_iter() {
-                self.write_value_tsv(cell_key_node, &value)?;
-
-                for sub_value in &value.versions {
-                    self.write_value_tsv(cell_key_node, sub_value)?;
-                }
-            }
-        }
-        Ok(())
-    }
-
-    fn get_sequence_num_string(seq_num: Option<u32>) -> String {
-        match seq_num {
-            Some(seq_num) => format!("{}", seq_num),
-            _ => String::new(),
-        }
     }
 }
