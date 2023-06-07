f8fb50861ca36df0af7160dc92900941bff51e42
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2023-06-07 16:10:32 +0100

    a - Formatting.

--- pynotatin/src/py_notatin_value.rs
+++ pynotatin/src/py_notatin_value.rs
@@ -142,7 +142,8 @@ impl PyNotatinValueVersionsIterator {
             Some(value) => {
                 self.index += 1;
                 Some(PyNotatinValuesIterator::reg_value_to_pyobject(
-                    value.clone(), py
+                    value.clone(),
+                    py
                 ))
             }
             None => None,
