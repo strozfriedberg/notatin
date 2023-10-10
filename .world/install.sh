d049040121db794950390b7fb94b3357268eb3fe
Shane McCulley <shane.mcculley@aon.com>
2023-10-10 12:38:40 -0500

    F - Remove package windows stuff, add release build

--- .world/install.sh
+++ .world/install.sh
@@ -2,19 +2,16 @@
 
 . .world/build_config.sh
 
-if [[ "$Linkage" == 'static' || ( "$Target" == 'windows' ) ]]; then
+if [[ "$Linkage" == 'static' || ("$Target" == 'windows') ]]; then
   exit
 fi
 
+pushd pynotatin
+poetry run maturin build --release --interpreter python --no-sdist
 mkdir -p $INSTALL/lib/python/pynotatin
-cp pynotatin/target/wheels/* $INSTALL/lib/python/pynotatin
+cp target/wheels/* $INSTALL/lib/python/pynotatin
+popd
 
 mkdir -p $INSTALL/bin
-cp target/release/reg_compare${EXE_EXT} $INSTALL/bin
-cp target/release/reg_dump${EXE_EXT} $INSTALL/bin
-
-if [[ "$Target" == 'windows_package' ]]; then
-  pushd pynotatin
-  pip install .
-  popd
-fi
+cp release/reg_compare${EXE_EXT} $INSTALL/bin
+cp release/reg_dump${EXE_EXT} $INSTALL/bin
