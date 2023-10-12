18a9c82a982b38dc6dca3a5a9154add1c3ea57ba
Shane McCulley <shane.mcculley@aon.com>
2023-10-12 10:10:13 -0500

    F - Dont forget the rest of the commit

--- .world/config.sh
+++ .world/config.sh
@@ -2,11 +2,14 @@
 
 . .world/build_config.sh
 
-if [[ "$Linkage" == 'static' || ( "$Target" == 'windows' ) ]]; then
+if [[ "$Linkage" == 'static' || ("$Target" == 'windows') ]]; then
   exit
 fi
 
+if [[ "$Target" == 'windows_package' ]]; then
+  export POETRY_CACHE_DIR=pynotatin/.poetry
 fi
+
 pushd pynotatin
 poetry lock --check && poetry install --no-cache
 popd
